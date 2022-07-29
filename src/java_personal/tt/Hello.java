c7506d54db184f665bb2001910a0ce90f9b763f5
Laelpark <gottkfkd@naver.com>
2022-07-29 14:57:13 +0900

    2022-07-29

--- src/java_personal/tt/Hello.java
+++ src/java_personal/tt/Hello.java
@@ -2,9 +2,9 @@ package java_personal.tt;
 
 public class Hello {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {    /* java 시작 무조건 기억하기 */
 		// TODO Auto-generated method stub
-		System.out.println("Hello world"); 
+		System.out.println("Hello world!!"); 
 	}
 
 }
